d9e40757f0e6b0230831c776fcebfbe83d8ab3be
MahtoSujeet <sujeetmehta6@gmail.com>
2025-01-31 02:19:10 +0530

    configured tailwind

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
+      <p className="text-2xl bg-red-500">Welcome to the home page!</p>
     </div>
   );
 }
