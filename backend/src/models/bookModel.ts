01b9a6a9f29984d915b76a8d75ba5d4ab5cf1aae
MahtoSujeet <sujeetmehta6@gmail.com>
2025-01-30 20:35:57 +0530

    add bookModel

---
+++ backend/src/models/bookModel.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IBook extends Document {
+  title: string;
+  pages: Schema.Types.ObjectId[];
+}
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  pages: [{ type: Schema.Types.ObjectId, ref: 'Page' }]
+});
+
+const Book = mongoose.model<IBook>('Book', bookSchema);
+
+export default Book;
+
