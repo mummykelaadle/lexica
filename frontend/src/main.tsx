49ba9f6427de01461954c772fde3bf8b8ec50078
DeshDeepakKushwaha <deshdeepakkushwah04@gmail.com>
2025-01-31 20:02:45 +0530

    Added Register and DashBoard Component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -14,7 +14,11 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+    <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />}/>
+          </Routes>
+        </BrowserRouter>
     </ClerkProvider>
   </StrictMode>,
 )
