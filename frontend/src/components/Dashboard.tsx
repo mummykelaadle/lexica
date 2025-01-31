4addd2e5cc4f67c4b29234e2feddae3deb1558f0
DeshDeepakKushwaha <deshdeepakkushwah04@gmail.com>
2025-01-31 22:57:52 +0530

    Added Header Componenet

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,12 +1,11 @@
-import { UserButton } from "@clerk/clerk-react";
+
+import Header from "./Header";
 
 export default function Dashboard(){
     return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="bg-white">
-                <UserButton afterSignOutUrl="/"/>
-            </div>
-           <h1 className="white">Dashboard</h1>
+        <div>
+            <Header />
         </div>
+       
     )
 }
