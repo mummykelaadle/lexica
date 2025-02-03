2f46bfbea039315799f172665c5642b8e10ca87c
MahtoSujeet <sujeetmehta6@gmail.com>
2025-02-03 17:09:56 +0530

    added endpoint to send file to

--- frontend/src/components/DropZone.tsx
+++ frontend/src/components/DropZone.tsx
@@ -1,14 +1,22 @@
 "use client";
 import { cn } from "@/lib/utils";
+import axios from "axios";
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
 
+const backendBaseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
+
 function DropZone() {
   const [file, setFile] = useState<File | null>(null);
+  const [isProcessing, setIsProcessing] = useState(false);
   const maxSize = 20971520; // Max file size (20 MB)
 
-  const onDrop = (acceptedFiles: File[]) => {
+  const onDrop = async (acceptedFiles: File[]) => {
     setFile(acceptedFiles[0]); // Store the first selected file
+    setIsProcessing(true);
+    const res = await axios.post(`${backendBaseUrl}/api/v1/pdf/upload`, acceptedFiles[0]);
+    setIsProcessing(false);
+    alert(res.data.message);
   };
 
   const { getRootProps, getInputProps, isDragActive, isDragReject, fileRejections } = useDropzone({
@@ -22,7 +30,7 @@ function DropZone() {
   const isFileTooLarge = fileRejections.length > 0 && fileRejections[0].file.size > maxSize;
   // Check if file is not PDF
   const isFileNotPdf = fileRejections.length > 0 && fileRejections[0].file.type !== "application/pdf";
-  
+
   // Check if multiple files were selected, even though multiple: false
   const isMultipleFiles = fileRejections.length > 1;
 
@@ -51,6 +59,9 @@ function DropZone() {
         <p>
           {isDragReject && isFileNotPdf && "File type not accepted. Only PDF files are allowed."}
         </p>
+        <p>
+          {isDragReject && isFileNotPdf && "File type not accepted. Only PDF files are allowed."}
+        </p>
         {/* Show message if multiple files are selected */}
         {isMultipleFiles && (
           <p >You can only upload one file at a time.</p>
@@ -60,6 +71,9 @@ function DropZone() {
       {/* File size warning */}
       {isFileTooLarge && (
         <div className="text-red-500 mt-2">File is too large. Max size is 20MB.</div>
+      )}
+      {isProcessing && (
+        <div className="text-red-500 mt-2">Processing.... Please wait!</div>
       )}
 
       {/* File name display */}
