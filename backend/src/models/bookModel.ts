a51ecaf875ebe932344cc2314f1444efa162e252
Rajat Shukla <rajatshukla1000@gmail.com>
2025-02-06 19:21:27 +0530

    added userId to book model

--- backend/src/models/bookModel.ts
+++ backend/src/models/bookModel.ts
@@ -1,11 +1,13 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 interface IBook extends Document {
+  ownerId:string
   title: string;
   pages: Schema.Types.ObjectId[];
 }
 
 const bookSchema = new Schema<IBook>({
+  ownerId:{ type: String, required: true },
   title: { type: String, required: true },
   pages: [{ type: Schema.Types.ObjectId, ref: 'Page' }]
 });
