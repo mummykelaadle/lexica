2ddf497e6d369e1acc4dd49eef3ae922db97a68e
MahtoSujeet <sujeetmehta6@gmail.com>
2025-02-07 15:48:05 +0530

    removed unsed imports and logs

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -1,10 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import logger from '../utils/logger';
 
-console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
-console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
-console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET);
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
