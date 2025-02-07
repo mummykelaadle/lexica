72fc15c91bd823ed850b6136655b5b23bd13527d
Rajat Shukla <rajatshukla1000@gmail.com>
2025-02-07 12:53:10 +0530

    added animation while books load

---
+++ frontend/src/animations/BooksLoading.tsx
@@ -0,0 +1,14 @@
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const BooksLoading = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/7f8217f3-f3ba-4cf1-bfe8-6f0313e4d798/YztiHXeRdU.lottie"
+      loop
+      autoplay
+      className="h-[400px]"
+    />
+  );
+};
+
+export {BooksLoading};
